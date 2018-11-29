63e9588c24345886f39f986a1b57cd5b8ba1d1b4
Matt Flowers <mattmflowers28@gmail.com>
2018-11-29 16:58:04 -0500

    Change configuration

--- server.js
+++ server.js
@@ -1,27 +1,46 @@
 const express = require('express');
-const path = require('path');
-const app = express();
-const bodyParser = require('body-parser');
+const helmet = require('helmet');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const path = require('path');
+const cluster = require('cluster');
+const workers = process.env.WEB_CONCURRENCY || require('os').cpus().length;
+const mongoose = require('mongoose');
 
-const port = process.env.PORT | 8080;
+const port = process.env.PORT || 8080;
+const loggingType = process.env.LOGGING_TYPE || 'dev';
 const clientPath = path.join(__dirname, 'client/build');
+const mongodbUri = process.env.MONGODB_URI || 'mongodb://user:password1@ds053459.mlab.com:53459/list-app-dev';
 
-app.use(express.static(clientPath));
+mongoose.connect(mongodbUri);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan(loggingType));
 app.use(cors());
+app.use(helmet());
+app.use(express.static(clientPath));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(clientPath, 'index.html'));
 });
 
-app.get('/api', (req, res) => {
-  res.json({
-    message: 'Welcome to my API!',
-  });
-});
+db.on('open', () => {
+  if (process.env.NODE_ENV === 'production' && cluster.isMaster) {
+    for (let i = 0; i < workers; i++) {
+      cluster.fork();
+    }
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+    cluster.on('exit', () => {
+      cluster.fork();
+    });
+  } else {
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  }
 });
